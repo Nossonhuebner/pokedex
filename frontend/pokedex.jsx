712098e270d66ea917a1ed7700ba12ba0d991ad3
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-07-04 15:20:12 -0400

    Implement store, with pokemon and entity reducers

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import configureStore from './store.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
   const rootEl = document.getElementById('root');
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
+
+
+  // TODO: DELETE FOR PRODUCTION
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 });
+
+  // TODO: DELETE FOR PRODUCTION
+import { fetchAllPokemon } from './util/api_util';
+import { RECEIVE_ALL_POKEMON, receiveAllPokemon } from './actions/pokemon_actions';
+
+window.fetchAllPokemon = fetchAllPokemon;
+window.receiveAllPokemon = receiveAllPokemon;
